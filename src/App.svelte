d2aa44a8377905eaf7171761ff189c6e220701b7
MalishaShah <41448279+MalishaShah@users.noreply.github.com>
2020-07-20 18:15:25 -0400

    Added some files and basic HTML skeleton structure of elements we will begin with

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<main>
+	<div id="header">
+		<img id="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Disk_pack1.svg/1200px-Disk_pack1.svg.png" alt="Down To Donate logo">
+		<div id="nav">
+			<ul>
+				<!--List of links that will be displayed. Using header as placeholder-->
+				<li><a href="#header">Map</a></li>
+				<li><a href="#header">What to do with...</a></li>
+				<li><a href="#header">Articles</a></li>
+				<li><a href="#header">About us</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="dashboard">
+		Dashboard here will scroll through images/pages highlighting aspects of website
+	</div>
+	<div id="locator">
+		<div id="map">Map element</div>
+		<div id="list">
+			<p>List of places</p>
+			<ol>
+				<li>Donation place 1</li>
+				<li>Donation place 2</li>
+				<li>Donation place 3</li>
+			</ol>
+		</div>
+	</div>
+	<h1>Hello</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	
+</style>
